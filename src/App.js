4d31797f5b7e5d2a88d6eb88d42bb3ead0862d66
medine yavuz <medineyldr.21@gmail.com>
2024-01-15 15:37:10 +0300

    dosyalar oluşturuldu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Todos from "./Components/Todos";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Todos></Todos>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Todos/Main/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+  return <div>MAİN</div>;
+}
+
+export default Main;
